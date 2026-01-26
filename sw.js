a12bf5908842b226dbfae83712b4ace90e7c1a62
Karl Altmannshofer <153896551+PiratKarl@users.noreply.github.com>
2026-01-26 08:31:45 +0100

    Add files via upload
    Signed-off-by: Karl Altmannshofer  <153896551+PiratKarl@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,17 +1,28 @@
-// AURA WEATHER SERVICE WORKER V2.1
-// Durch Ändern dieses Namens zwingen wir das Tablet zum Update:
-var cacheName = 'aura-weather-v2.1-hybrid';
+// AURA WEATHER SERVICE WORKER V2.9 - TICKER TALK EDITION
+// Das Hochsetzen der Version zwingt das Tablet, alle Dateien neu zu laden.
+var cacheName = 'aura-weather-v2.9-ticker-talk';
 
+// Hier listen wir alles auf, was "unkaputtbar" auf dem Tablet liegen soll:
 var filesToCache = [
   './',
   './index.html',
   './style.css',
   './script.js',
-  './logo.png'
+  './manifest.json',
+  './logo.png',
+  // Hier die gängigsten Wetter-Icons, damit sie offline funktionieren:
+  './01d.gif', './01n.gif',
+  './02d.gif', './02n.gif',
+  './03d.gif', './03n.gif',
+  './04d.gif', './04n.gif',
+  './09d.gif', './09n.gif',
+  './10d.gif', './10n.gif',
+  './11d.gif', './11n.gif',
+  './13d.gif', './13n.gif',
+  './50d.gif', './50n.gif'
 ];
 
 self.addEventListener('install', function(e) {
-  // Alten Cache sofort ersetzen
   self.skipWaiting();
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
@@ -21,7 +32,6 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) {
-  // Alte Caches löschen, die nicht mehr aktuell sind
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
@@ -37,7 +47,7 @@ self.addEventListener('activate', function(e) {
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
-      // Entweder aus dem Cache laden ODER neu aus dem Netz holen
+      // Priorität: Cache (blitzschnell), falls nicht da: Netzwerk.
       return response || fetch(e.request);
     })
   );
